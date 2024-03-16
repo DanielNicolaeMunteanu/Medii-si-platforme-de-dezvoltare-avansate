daa1abad998d6afd46390621e4f73fc376c6399a
DanielNicolaeMunteanu <152066849+DanielNicolaeMunteanu@users.noreply.github.com>
2024-03-16 12:11:33 +0200

    Implemenet Animal Sounds

---
+++ MPDA_proiect1/MPDA_proiect1/Cat.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Cat : IAnimal
+{
+    public string ProduceSound()
+    {
+        return "Meow";
+    }
+}
---
+++ MPDA_proiect1/MPDA_proiect1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IAnimal> animals = new List<IAnimal>
+        {
+            new Dog(),
+            new Cat(),
+            new Bird()
+        };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"{animal.GetType().Name} make sounds: {animal.ProduceSound()}");
+        }
+    }
+}
